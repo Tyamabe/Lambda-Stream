8c9d142287926862884578d57ca1be5fdc912d21
TaiheiYamabe <91378148+Tyamabe@users.noreply.github.com>
2024-03-30 09:30:07 +0900

    新規Javaプロジェクト作成

---
+++ src/Task.java
@@ -0,0 +1,22 @@
+import java.time.LocalDate;
+
+
+public class Task {
+	//日付を表すdと行動を表すactionの変数を用意する
+	private LocalDate date;
+	private String action;
+	//コンストラクタ
+	public Task(LocalDate date, String action) {
+		this.date = date;
+		this.action = action;
+	}
+	//getter作っておく
+	public LocalDate getDate() {
+		return this.date;
+	}
+	
+	public String getAction() {
+		return this.action;
+	}
+	
+}
